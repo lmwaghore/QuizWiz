c0ef1566a718ab5b9ced5e01272cf02c650d5807
Lerroy Mwaghore <lmwaghore@gmail.com>
2021-06-26 11:52:46 -0500

    Set up Pt1

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import QuestionCard from './components/QuestionCard'
+
+const App = () => {
+  return (
+    <div className="App">
+      Quiz Goes Here
+      <div>
+        <QuestionCard/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const QuestionCard = () => {
+  return <div>Question Card Goes Here</div>
+}
+
+export default QuestionCard;
