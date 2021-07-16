3249e3f9ca1a680df3f95ce44605e786632a225b
Lerroy Mwaghore <lmwaghore@gmail.com>
2021-07-16 10:33:46 -0500

    Update

---
+++ src/components/QuestionCard.styles.ts
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+
+export const Wrapper = styled.div`
+  max-width: 1100px;
+  background: #ebfeff;
+  border-radius: 10px;
+  border: 2px solid #008a3;
+  padding: 20px;
+  box-shadow: 0px 5pxx 10px rgba(0, 0, 0, 0.25);
+  text-align: center;
+
+  p{
+    font-size: 1rem;
+  }
+`
+
+type ButtonWrapperProps = {
+  correct: boolean;
+  playerClicked: boolean;
+}
+
+export const ButtonWrapper = styled.div<ButtonWrapperProps>`
+  transition: all 0.3s ease;
+
+  :hover {
+    opacity: 0.8;
+  }
+
+  button {
+    cursor: pointer;
+    user-selected: none;
+    font-size: 0.8 rem;
+    width: 100%;
+    height: 40px;
+    margin: 5px 0;
+    background: ${({correct, playerClicked}) =>
+      correct
+      ? 'linear-gradient(90deg, #56ffa4, #59bc86)'
+      : !correct && playerClicked
+      ? 'linear-gradient(90deg, #ff5656, #c16868)'
+      : 'linear-gradient(90deg, #56ccff, #6eafb4)'
+    };
+    border: 3px solid #fff;
+    box-shadow: 1px 2px 0px rgba(0, 0, 0, 0.1);
+    border-radius: 10px;
+    color: #fff;
+    text-shadow: 0px 1px 0px rgba(0, 0, 0, 0.25)
+  }
+
+`
